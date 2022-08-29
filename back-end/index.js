443c37efe7139f856678e33ac0f3c545480ea70a
Joshua Levinson <josh@192-168-1-106.tpgi.com.au>
2022-08-29 11:26:30 +1000

    prep for heroku deployment

--- back-end/index.js
+++ back-end/index.js
@@ -1,15 +1,17 @@
 require('dotenv').config()
-
+// ----------------------------------------------------------- //
+// Require dependencies
 const express = require('express')
 const mongoose = require('mongoose')
 const session = require('express-session')
 const mongoDBSession = require('connect-mongodb-session')
-// const cors = require('cors')
-
+// ----------------------------------------------------------- //
+// Require controllers
 const recipesController = require('./controllers/Recipes');
 const mealPlanController = require('./controllers/MealPlan')
 const userController = require('./controllers/Users');
-
+// ----------------------------------------------------------- //
+// Session
 const app = express()
 const PORT = process.env.PORT
 const dbURL = process.env.MONGODB_URL
@@ -18,11 +20,7 @@ const sessionStore = new MongoDBStore({
   uri: dbURL,
   collection: 'sessions'
 })
-
-// app.use(cors({
-//   origin: '*'
-// }));
-
+// ----------------------------------------------------------- //
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
@@ -32,25 +30,31 @@ app.use(session({
     maxAge: 7 * 24 * 60 * 60 * 1000
   }
 }))
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-// app.use(express.static(__dirname + "/client/build"))
-
-
+// ----------------------------------------------------------- //
+// Use the production version of app (located in build folder in client)
+app.use(express.static(__dirname + '/client/build'))
+// ----------------------------------------------------------- //
+// Default routes
 app.use('/', recipesController)
 app.use('/meals', mealPlanController)
 app.use('/user', userController)
-// app.get("*", (req, res) => {
-//   res.sendFile(__dirname + "/client/build/index.html")
-// })
-
-
+// ----------------------------------------------------------- //
+// Catch all requests
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + '/client/build/index.html')
+  // Entry point for react app ->
+  // Send client/build/index.html
+})
+// ----------------------------------------------------------- //
+// Connect to database
 mongoose.connect(dbURL,() => {
   console.log('connected to MongoDB')
 })
 
+// Connect to port
 app.listen(PORT, () => {
   console.log('listening on port: ', PORT)
 })
+// ----------------------------------------------------------- //
